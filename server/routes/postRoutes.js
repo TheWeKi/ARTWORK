70373f2864221a99bf808bbe32a7af44037d61a4
Rituraj Pandey <68937122+TheWeKi@users.noreply.github.com>
2023-07-18 15:46:20 +0530

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -26,7 +26,9 @@ router.route('/').get(async (req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { name, prompt, photo } = req.body;
-    const photoUrl = await cloudinary.uploader.upload(photo);
+    const photoUrl = await cloudinary.uploader.upload(photo, {
+      format: 'jpg',
+    });
 
     const newPost = await Post.create({
       name,
